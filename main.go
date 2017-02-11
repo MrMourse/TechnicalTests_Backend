2dbe4642d3a37e444d6a3533c18cbf2125643da5
Paoli Raphael <paoli.ensiie@gmail.com>
2017-02-11 10:18:34 +0100

    Create_web_server_#1
    Simple helloworld function to try if it works.

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"io"
+	"net/http"
+)
+
+func hello(w http.ResponseWriter,r *http.Request){
+	io.WriteString(w,"Hello world !")
+}
+
+var mux map[string]func(http.ResponseWriter, *http.Request)
+
+type myHandler struct{}
+
+
+func (*myHandler) ServeHTTP(w http.ResponseWriter, r *http.Request){
+	if h, ok := mux[r.URL.String()]; ok{
+		h(w,r)
+		return
+	}
+
+	io.WriteString(w , "My server: "+ r.URL.String())
+}
+
+
+func main() {
+
+	server := http.Server{
+		Addr: ":8000",
+		Handler: &myHandler{},
+	}
+
+	mux = make(map[string]func(http.ResponseWriter, *http.Request))
+	mux["/"] = hello
+
+	//manage request
+	server.ListenAndServe()
+
+
+}
