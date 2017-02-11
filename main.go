d4a4d38643dd86695611e1937d9c2576d2726f94
Paoli Raphael <paoli.ensiie@gmail.com>
2017-02-11 18:34:14 +0100

    Version with method post tested #1 #2

--- main.go
+++ main.go
@@ -3,39 +3,83 @@ package main
 import (
 	"log"
 	"net/http"
+	"strings"
+	"flag"
+	"github.com/google/go-github/github"
 	"fmt"
 	"html/template"
 )
 
-func Home( w http.ResponseWriter, req *http.Request){
-	render(w,"index.html")
-}
-func Search( w http.ResponseWriter, req *http.Request){
-	render(w, "result.html")
+
+type Content struct{
+	Path string
+	Synopsis string
+	Language string
+	Length float64
+	GitHubFullName string
 }
 
-func render(w http.ResponseWriter, tmpl string){
-	tmpl =  fmt.Sprintf("templates/%s", tmpl)
-	t, err := template.ParseFiles(tmpl)
-	if err != nil {
-		log.Print("template parsing error: ", err)
+func request(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("method:", r.Method) //get request method
+	if r.Method == "GET" {
+		t, _ := template.ParseFiles("index.html")
+		t.Execute(w, nil)
+	} else {
+		r.ParseForm()
+		// logic part of log in
+		fmt.Println("language: ", r.Form["language"])
 	}
-	err = t.Execute(w, "")
-	if err!= nil {
-		log.Print("template executing error: ", err)
-	}
+
 }
 
 func main() {
 
-	http.HandleFunc("/", Home)
-	http.HandleFunc("/search", Search)
+	//On initialise la valeur du port
+	port := flag.String("port", "3000", "server port number")
+
+	http.HandleFunc("/", request)
+
+	http.HandleFunc("/search", func(w http.ResponseWriter, r *http.Request) {
+
+		language := strings.TrimSpace(r.URL.Query().Get("language"))
+
+
+		if language != "" {
+
+			log.Println("Query: ", language)
+		}
+
+		client := github.NewClient(nil)
+
+		fmt.Println("Repos that contain the query language.")
+
+		query := fmt.Sprintf("language:"+language)
+
+		opts := &github.SearchOptions{
+			Sort: "stars",
+			ListOptions: github.ListOptions{
+				PerPage: 100,
+			},
+		}
+
+		repos, _, err := client.Search.Repositories(query, opts)
+
+		if err != nil {
+			fmt.Printf("error: %v\n\n", err)
+		} else {
+			fmt.Printf("%v\n\n", github.Stringify(repos))
+		}
+
+	})
+
+	log.Println("Listening on :" + *port)
 	//manage request
 	log.Println("Listenning...")
-	err := http.ListenAndServe(":3001", nil)
+	err := http.ListenAndServe(":" + *port, nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
-
-
 }
+
+
+
