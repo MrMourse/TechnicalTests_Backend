a68c68151f37a1b3336e0afbc983b81629d81b44
Paoli Raphael <paoli.ensiie@gmail.com>
2017-02-12 19:19:49 +0100

    Collecting done #4,  data gathered #5 improvement with api function for the process

--- main.go
+++ main.go
@@ -3,64 +3,40 @@ package main
 import (
 	"log"
 	"net/http"
-/*	"strings"*/
 	"flag"
-/*	"github.com/google/go-github/github"*/
 	"fmt"
 	"html/template"
-/*	"os"
-	"io"
-	"strings"*/
-	"io/ioutil"
-	/*
-	"bytes"
-	*/
-
-	"strings"
-	/*"strconv"*/
-	"strconv"
+	"github.com/google/go-github/github"
+	"golang.org/x/oauth2"
 )
 
-func  get_body(URL string) string {
-	response, err := http.Get(URL)
-	if err != nil {
-		log.Fatal(err)
-	}
-	body, err := ioutil.ReadAll(response.Body)
-	response.Body.Close()
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("body content : %s", body)
-	/*n := bytes.Index(body, []byte{0})*/
-	s := string(body[:])
-	return s
-}
-/*return an hash table*/
-func hash_table(body string) map[string]int{
 
-	/*s := '{"C++":20754080,"Python":17160192,"Jupyter Notebook":1833840,"TypeScript":769672,"Go":763825}'*/
-	tmp := strings.Split(body, "{")
-	res := tmp[1]
-	tmp = strings.Split(res,"}")
-	res = tmp[0]
-	tmp = strings.Split(res, ",")
-	m:= make(map[string]int)
-	for elmt := range tmp  {
-		tab := strings.Split(tmp[elmt], ":")
-		tmp := strings.Split(tab[0], "\"")
-		value, _ := strconv.Atoi(tab[1])
-		m[tmp[1]] = value
-	}
-
-	return m
-}
 
 func request(w http.ResponseWriter, r *http.Request) {
 
 	t, _ := template.ParseFiles("index.html")
 	t.Execute(w, nil)
+}
+
+type repos_hash struct{
+	url string
+	hashtable map[string]int
+}
+
+func gather(allRepos []*github.Repository,client *github.Client) []repos_hash{
+	var result []repos_hash
+	var repos_tmp repos_hash
+	for _,repo := range allRepos {
+		//filter them
+		fmt.Printf( "repo_URL : %s \n",*repo.URL)
+		res, _, err := client.Repositories.ListLanguages(*repo.Owner.Login,*repo.Name)
+		if err != nil {fmt.Println(err)}
+		repos_tmp.hashtable = res
+		repos_tmp.url = *repo.URL
+		result= append(result,repos_tmp)
 	}
+	return result
+}
 
 func search(w http.ResponseWriter, r *http.Request) {
 	/*Get the form content*/
@@ -69,37 +45,31 @@ func search(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("language: ", r.Form["language"])
 /*	language := strings.Join(r.Form["language"], "")*/
 
+	//start the query on github
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: "b1abf4e31f153ad21e19cf70dabce2310a731b1c"},
+	)
+	tc := oauth2.NewClient(oauth2.NoContext, ts)
 
-/*	client := github.NewClient(nil)
+	client := github.NewClient(tc)
 
-	fmt.Println("Repos that contain the query language.")
-
-	*//*query := fmt.Sprintf("language:" +  language)*//*
 	//get the 100 last repositories
 	opt := &github.RepositoryListAllOptions{
 		ListOptions: github.ListOptions{PerPage: 100},
 	}
+
 	// get all pages of results
 	var allRepos []*github.Repository
 	repos, _, err := client.Repositories.ListAll(opt)
-	if (err !=nil){
+	if (err !=nil) {
 		fmt.Println(err)
 	}
-	//gather them
 	allRepos = append(allRepos, repos...)
-	for _, repo := range allRepos {
-	//filter them
+	allres := gather(allRepos,client)
+	for _,elmt:=range allres  {
+		fmt.Printf("elmt :%s\n",elmt.url)
+	}
 
-		fmt.Println("repo: ", *repo.FullName)
-		fmt.Println("owner: ", *repo.Owner.Login)
-		fmt.Println("languages:", * repo.LanguagesURL)
-
-	}*/
-	//URL treatment
-	body := get_body("http://api.github.com/repos/tensorflow/tensorflow/languages")
-	fmt.Printf("s content : %s\n", body)
-	res := hash_table(body)
-	fmt.Printf("result : %d\n",res["C++"])
 	t, _ := template.ParseFiles("result.html")
 	t.Execute(w, nil)
 }
@@ -114,9 +84,11 @@ func main() {
 	http.HandleFunc("/search", search)
 
 	log.Println("Listening on :" + *port)
+
 	//manage request
 	log.Println("Listenning...")
 	err := http.ListenAndServe(":" + *port, nil)
+
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
